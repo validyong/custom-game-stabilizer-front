c8f3acfb5dfbbffc1917117a434e41ec6521958b
validyong <rokudynamics@gmail.com>
2022-12-08 17:37:23 +0900

    :tada: create project

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  purge: ["./index.html",'./src/**/*.{svelte,js,ts}'],
+  variants: {
+    extend: {},
+  }
+}
